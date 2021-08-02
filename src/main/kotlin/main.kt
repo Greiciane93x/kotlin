a95e098766e67af1d3be866ee919f8c68104dd2d
Greiciane93x <greiciane_araujo@hotmail.com>
2021-08-02 15:58:57 -0300

    estrutura for

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,37 +1,38 @@
 fun main(){
     println("Bem vindo ao kotlin...")
     // var pode ser modificada, val não
-    val titular: String = "Ane"
+
 //    titular = "Teste imutabilidade" [erro de compilação]
 //    print(titular)
 
+    for(i in 1..5){
+            val titular: String = "Ane $i"
+            val numeroConta: Int = 101010 + i
+            var saldo: Double = i + 10.0
+            //saldo += 5000
+            // saldo -= 100000
+            // com string template
+            // println("titular: " + titular)
+            // sem string template
+            println("titular:  $titular")
+            println("numeroConta:  $numeroConta")
+            println("saldo:  $saldo")
+            println()
 
-    val numeroConta: Int = 101010
-    var saldo: Double = 0.0
-    saldo += 5000
-    saldo -= 100000
-    // com string template
-    // println("titular: " + titular)
-    // sem string template
-    println("titular:  $titular")
-    println("numeroConta:  $numeroConta")
-    println("saldo:  $saldo")
-
-
-    if(saldo > 0.0){
-        println("conta é positiva")
-    }else if(saldo == 0.0){
-        println("conta é neutra")
-    }else{
-        println("conta é negativa")
-    }
-
-    // similar ao switch case
-    when {
-        saldo > 0.0 -> println("conta é positiva")
-        saldo == 0.0 -> println("conta é neutra")
-        else -> {
-            println("conta é negativa")
+                if(saldo > 0.0){
+                    println("conta é positiva")
+                }else if(saldo == 0.0){
+                    println("conta é neutra")
+                }else{
+                    println("conta é negativa")
+                }
         }
-    }
+    // similar ao switch case
+//    when {
+//        saldo > 0.0 -> println("conta é positiva")
+//        saldo == 0.0 -> println("conta é neutra")
+//        else -> {
+//            println("conta é negativa")
+//        }
+//    }
 }
