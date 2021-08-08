1c0c725ffd79713d8ecbed632a50c0c89c5a0947
Greiciane93x <greiciane_araujo@hotmail.com>
2021-08-08 18:18:25 -0300

    explorando higher order functions, utilizando a interface autenticavel

--- src/main/kotlin/br/com/alura/bytebank/modelo/SistemaInterno.kt
+++ src/main/kotlin/br/com/alura/bytebank/modelo/SistemaInterno.kt
@@ -2,9 +2,10 @@ package br.com.alura.bytebank.modelo
 
 class SistemaInterno {
 
-    fun entra(admin: br.com.alura.bytebank.modelo.Autenticavel, senha: Int){
+    fun entra(admin: br.com.alura.bytebank.modelo.Autenticavel, senha: Int, autenticado: () -> Unit = {}){
         if(admin.autentica(senha)){
             println("Bem vindo ao Bytebank")
+            autenticado()
         }else{
             println("Falha na autenticação")
         }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,6 @@
+import br.com.alura.bytebank.modelo.Autenticavel
 import br.com.alura.bytebank.modelo.Endereco
+import br.com.alura.bytebank.modelo.SistemaInterno
 
 fun main() {
 
@@ -10,11 +12,18 @@ fun main() {
         val tamanhoComplemento: Int = it.complemento?.length ?:0
         println(tamanhoComplemento)
     }
+
+
+
+
+
+    val autenticavel  = object : Autenticavel{
+        val senha = 1234
+        override fun autentica(senha: Int)  = this.senha == senha
+    }
+
+    SistemaInterno().entra(autenticavel, 1234, autenticado = {
+        println("realizar pagamento") })
+
 }
-
-fun teste(endereco: Endereco){
-
-}
-
-
 
